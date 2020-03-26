cbc2c590588a257260bb2ca8f908bb2cdebb27cc
Sam Grundman <samuel@yodas.ws>
2020-03-25 20:33:52 -0400

    Display a beautiful track in the middle of the screen

--- src/app.js
+++ src/app.js
@@ -35,15 +35,56 @@ function RaceTrack(svg) {
 	const gradients = [
 		{
 			gradient: [1, 0],
-			width: 5,
+			delta: [0, 0],
+			width: 20,
+		},
+		{
+			gradient: [1, 1],
+			delta: [27, 13],
+			width: 20,
+		},
+		{
+			gradient: [0, 1],
+			delta: [13, 27],
+			width: 20,
+		},
+		{
+			gradient: [-1, 1],
+			delta: [-13, 27],
+			width: 20,
+		},
+		{
+			gradient: [-1, 0],
+			delta: [-27, 13],
+			width: 20,
+		},
+		{
+			gradient: [-1, 0],
+			delta: [-120, 0],
+			width: 20,
+		},
+		{
+			gradient: [1, -1],
+			delta: [-27, -13],
+			width: 20,
+			up: true,
+		},
+		{
+			gradient: [0, -1],
+			delta: [-13, -27],
+			width: 20,
+			up: true,
+		},
+		{
+			gradient: [-1, -1],
+			delta: [13, -27],
+			width: 20,
+			up: true,
 		},
 		{
 			gradient: [1, 0],
-			width: 7,
-		},
-		{
-			gradient: [1, 0],
-			width: 10,
+			delta: [27, -13],
+			width: 20,
 		},
 	];
 
@@ -65,18 +106,75 @@ Object.defineProperties(RaceTrack.prototype, {
 		enumerable: true,
 		value() {
 			let buildPosition = [0, 0];
-			let buildOrientation = [1, 0];
-			this.gradients.forEach((grad) => {
-				// Set Standard Start Line
-				const line = document.createElementNS(SVG, 'line');
-				line.setAttribute('x1', buildPosition[x]);
-				line.setAttribute('x2', buildPosition[x]);
-				line.setAttribute('y1', buildPosition[y] - grad.width);
-				line.setAttribute('y2', buildPosition[y] + grad.width);
-				line.classList.add('checkpoint');
-				this.svg.appendChild(line);
-				buildPosition[x] += 10;
+			const extrema = [[0, 0], [0, 0]];
+			const rails = [[], []];
+
+			this.gradients.forEach((grad, i) => {
+				// Move build position
+				buildPosition[x] += grad.delta[x];
+				buildPosition[y] += grad.delta[y];
+
+				// Rotate to gradient
+				let α = Math.acos(grad.gradient[x] / Math.hypot(...grad.gradient));
+				if (grad.up) α += Math.PI;
+				const points = {
+					x1: buildPosition[x] + grad.width * Math.sin(α),
+					y1: buildPosition[y] - grad.width * Math.cos(α),
+					x2: buildPosition[x] - grad.width * Math.sin(α),
+					y2: buildPosition[y] + grad.width * Math.cos(α),
+				};
+
+				// Draw line
+				const elLine = document.createElementNS(SVG, 'line');
+				rails[0].push([points.x1, points.y1]);
+				rails[1].push([points.x2, points.y2]);
+
+				extrema[x][0] = Math.min(extrema[x][0], points.x1, points.x2);
+				extrema[x][1] = Math.max(extrema[x][1], points.x1, points.x2);
+				extrema[y][0] = Math.min(extrema[y][0], points.y1, points.y2);
+				extrema[y][1] = Math.max(extrema[y][1], points.y1, points.y2);
+
+				Object.entries(points).forEach(([attr, num]) => {
+					elLine.setAttribute(attr, num);
+				});
+				elLine.classList.add('checkpoint');
+				if (i === 0) {
+					elLine.classList.add('start-line');
+				}
+				this.svg.appendChild(elLine);
 			});
+
+			rails.forEach((rail) => {
+				rail.push(rail[0]);
+				const elLine = document.createElementNS(SVG, 'path');
+				const d = [];
+				rail.forEach((point) => {
+					d.push(point.join(','));
+				});
+				elLine.setAttribute('d', 'M' + d.join('L'));
+				this.svg.appendChild(elLine);
+			});
+
+			console.log('Sam, extrema:', extrema);
+			let width = extrema[x][1] - extrema[x][0] + 20;
+			let height = extrema[y][1] - extrema[y][0] + 20;
+			let x0 = extrema[x][0] - 10;
+			let y0 = extrema[y][0] - 10;
+
+			if (width > height) {
+				height = width * 4 / 5;
+				y0 = (extrema[y][0] + extrema[y][1] - height) / 2;
+			} else {
+				width = height * 5 / 4;
+				x0 = (extrema[x][0] + extrema[x][1] - height) / 2;
+			}
+
+			this.svg.setAttribute(
+				'viewBox',
+				`${x0} ${y0}
+				${width} ${height}`
+				.replace(/\s+/g, ' '),
+			);
 		},
 	},
 });
