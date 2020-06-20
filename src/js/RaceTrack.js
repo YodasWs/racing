d686cf5fc8a09907696cd17066e3674ea3eb9ef1
Sam Grundman <samuel@yodas.ws>
2020-06-20 12:50:16 -0400

    Move RaceTrack to separate files

---
+++ src/js/RaceTrack.js
@@ -0,0 +1,475 @@
+const SVG = 'http://www.w3.org/2000/svg';
+const TrackPiece = require('../js/TrackPiece');
+const x = 0;
+const y = 1;
+const z = 2;
+
+function RaceTrack(svg, track, cars, options = {}) {
+	const simulation = d3.forceSimulation();
+
+	Object.assign(this, {
+		laps: 10,
+	}, options, {
+		gradients: [],
+		rails: [],
+		tick: 0,
+		time: 0,
+		finalStanding: [],
+	});
+
+	const gTrack = document.createElementNS(SVG, 'g');
+	gTrack.setAttribute('id', 'gTrack');
+
+	const gCars = document.createElementNS(SVG, 'g');
+	gCars.setAttribute('id', 'gCars');
+
+	if (svg instanceof SVGElement) {
+		svg.appendChild(gTrack);
+		svg.appendChild(gCars);
+	}
+
+	Object.defineProperties(this, {
+		simulation: {
+			get: () => simulation,
+		},
+		svg: {
+			get: () => svg,
+		},
+		gTrack: {
+			get: () => gTrack,
+		},
+		gCars: {
+			get: () => gCars,
+		},
+	});
+
+	if (Array.isArray(track)) {
+		this.setTrack(track);
+	}
+	if (Array.isArray(cars)) {
+		this.setCars(cars);
+	}
+}
+
+RaceTrack.fromJson = (json, svg = null) => {
+	if (!(svg instanceof SVGElement)) {
+		svg = document.querySelector('svg');
+	}
+
+	// function RaceTrack(svg, track, cars, options)
+	return new RaceTrack(
+		svg,
+		json.gradients.map(piece => new TrackPiece(piece)),
+		json.cars,
+		{
+			laps: json.laps,
+		}
+	);
+};
+
+Object.defineProperties(RaceTrack.prototype, {
+	init: {
+		enumerable: true,
+		value() {
+			if (!(this.svg instanceof SVGElement)) {
+				throw new Error('RaceTrack.init requires valid svg element!');
+			}
+
+			if (!this.svg.getElementById('gTrack')) {
+				this.svg.appendChild(this.gTrack);
+			}
+			if (!this.svg.getElementById('gCars')) {
+				this.svg.appendChild(this.gCars);
+			}
+			this.simulation.force('fCollide', d3.forceCollide(car => car.radius));
+			const forceRailingBounce = require('../js/forceRailingBounce');
+			this.simulation.force('fRailing', forceRailingBounce(this.rails));
+
+			// Place Cars on Starting Line
+			this.simulation.nodes().forEach((car, i) => {
+				car.lapTimes = [];
+				car.x = -7 * (i + 1);
+				car.y = 5 * Math.pow(-1, i);
+				// Start with some velocity to increase excitement at the start
+				car.vx = 0.9;
+				car.vy = 0;
+				car.trackAhead = this.gradients.slice();
+				car.posn = [];
+			});
+			this.moveCars();
+			this.listCars();
+			this.simulation.alphaDecay(0);
+			this.simulation.velocityDecay(0.01);
+
+			this.simulation.on('tick', () => {
+				this.onTick();
+			});
+
+			return this;
+		},
+	},
+	onTick: {
+		value() {
+			this.simulation.nodes(this.simulation.nodes());
+			requestAnimationFrame((time) => {
+				this.time = time;
+			});
+			this.tick++;
+			this.moveCars();
+			this.listCars();
+			if (this.cars.every(c => c.lapTimes.length > this.laps)) {
+				this.simulation.stop();
+
+				// Remove circular reference for conversion to JSON
+				this.cars.forEach((car) => {
+					delete car.sphere;
+				});
+				const link = document.createElement('a');
+				link.setAttribute('href', `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(this))}`);
+				const now = new Date();
+				link.setAttribute('download', `race-${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}.json`);
+				link.innerText = 'Save race data';
+				document.querySelector('form').appendChild(link);
+
+				/*
+				document.querySelectorAll('svg, ol, form').forEach((el) => {
+					el.setAttribute('hidden', true);
+				});
+				document.querySelector('canvas#replay').removeAttribute('hidden');
+				/**/
+
+				buildReplay(this);
+				return;
+			}
+			return this;
+		},
+	},
+	moveCars: {
+		value() {
+			// Move Cars!
+			d3.selectAll('#gCars circle').attr('cx', d => d.x).attr('cy', d => d.y);
+			this.simulation.nodes().forEach((car, i, nodes) => {
+
+				/*
+				if (this.tick % 5 === 0) {
+					const lastTime = car.time.slice(-1).pop();
+					// console.log('Sam,', this.tick, `${car.name[0]}:${car.lapTimes.length}:${piece}`);
+					if (lastTime) console.log('Sam,', this.tick, `${car.name[0]}:${lastTime.lap}:${lastTime.piece}`);
+				}
+				/**/
+
+				if (car.nextPiece) {
+					// Add to time-tracking only on first crossover in the lap
+					const piece = this.gradients.indexOf(car.trackAhead[0]);
+					// TODO: Why is this sometimes not grabbing the time object?!
+					// TODO: What could cause an extra push to lapTimes?!
+					if (!car.time.some(t => t.lap === car.lapTimes.length && t.piece === piece)) {
+						car.time.push({
+							// TODO: point to index of car.posn with same tick
+							lap: car.lapTimes.length,
+							piece,
+							tick: this.tick,
+							time: this.time,
+						});
+
+						// Cross the start/end line, next lap
+						if (piece === 0) {
+							if (!this.finalStanding.includes(car.name)) {
+								const lastTime = car.time.slice(-1).pop();
+								// if (lastTime) console.log('Sam, next lap!', this.tick, `${car.name[0]}:${lastTime.lap}:${lastTime.piece}`);
+								car.lapTimes.push({
+									tick: this.tick,
+									time: this.time,
+								});
+							}
+
+							if (car.lapTimes.length > this.laps) {
+								// Finished!
+								if (!this.finalStanding.includes(car.name)) {
+									this.finalStanding.push(car.name);
+								}
+							}
+						}
+					}
+
+					car.trackAhead.push(car.trackAhead.shift());
+					car.nextPiece = false;
+				}
+
+				if (car.previousPiece) {
+					car.trackAhead.unshift(car.trackAhead.pop());
+					car.previousPiece = false;
+				}
+
+				// We finished race, so let's stop
+				if (this.finalStanding.includes(car.name)) {
+					const place = this.finalStanding.indexOf(car.name);
+
+					car.y = Math.min(Math.max(-20, car.y), 20);
+
+					if (car.x > this.gradients[0].x + (this.cars.length - place) * 10) {
+						car.fx = car.x;
+						car.fy = car.y;
+					}
+				}
+
+				// Off track? Stop!
+				if (car.x < this.extrema[x][0] - 10
+					|| car.x > this.extrema[x][1] + 10
+					|| car.y < this.extrema[y][0] - 10
+					|| car.y > this.extrema[y][1] + 10) {
+					this.simulation.stop();
+				}
+			});
+
+			// Record positional data for future replay
+			this.cars.forEach((car) => {
+				car.posn.push({
+					// TODO: if item was added to car.time, point to it
+					tick: this.tick,
+					time: this.time,
+					vx: car.vx,
+					vy: car.vy,
+					x: car.x,
+					y: car.y,
+				});
+			});
+
+			return this;
+		},
+	},
+	setTrack: {
+		enumerable: true,
+		value(track) {
+			this.gradients = track.filter(piece => piece instanceof TrackPiece);
+
+			// Add Track Pieces to SVG
+			let buildPosition = [0, 0];
+			const extrema = [[0, 0], [0, 0]];
+			const railPoints = [[], []];
+			this.rails = new Array(2).fill({});
+
+			const centerPoints = [];
+
+			this.gTrack.innerHTML = '';
+			this.gradients.forEach((grad, i) => {
+				// Move build position
+				buildPosition[x] += grad.delta[x];
+				buildPosition[y] += grad.delta[y];
+
+				centerPoints.push([buildPosition[x], buildPosition[y]]);
+
+				grad.x = buildPosition[x];
+				grad.y = buildPosition[y];
+
+				// Add track forces to simulation
+				if (grad.force) {
+					this.simulation.force(`piece${i}`, grad.force);
+				}
+
+				// Rotate to gradient
+				let α = Math.acos(grad.gradient[x] / Math.hypot(...grad.gradient));
+				if (grad.gradient[y] < 0) {
+					α = 2 * Math.PI - α;
+				}
+				const points = {
+					x1: buildPosition[x] + grad.width * Math.sin(α),
+					y1: buildPosition[y] - grad.width * Math.cos(α),
+					x2: buildPosition[x] - grad.width * Math.sin(α),
+					y2: buildPosition[y] + grad.width * Math.cos(α),
+				};
+
+				grad.m = (points.y2 - points.y1) / (points.x2 - points.x1);
+				// y - y1 = m (x - x1); y = mx - mx1 + y1
+				grad.b = -grad.m * points.x1 + points.y1;
+
+				// Draw line
+				const elLine = document.createElementNS(SVG, 'line');
+				railPoints[0].push({
+					before: [
+						points.x1 - grad.width * Math.cos(α) * grad.rail[0],
+						points.y1 - grad.width * Math.sin(α) * grad.rail[0],
+					],
+					is: [
+						points.x1,
+						points.y1,
+					],
+					after: [
+						points.x1 + grad.width * Math.cos(α) * grad.rail[0],
+						points.y1 + grad.width * Math.sin(α) * grad.rail[0],
+					],
+				});
+				railPoints[1].push({
+					before: [
+						points.x2 - grad.width * Math.cos(α) * grad.rail[1],
+						points.y2 - grad.width * Math.sin(α) * grad.rail[1],
+					],
+					is: [
+						points.x2,
+						points.y2,
+					],
+					after: [
+						points.x2 + grad.width * Math.cos(α) * grad.rail[1],
+						points.y2 + grad.width * Math.sin(α) * grad.rail[1],
+					],
+				});
+
+				extrema[x][0] = Math.min(extrema[x][0], points.x1, points.x2);
+				extrema[x][1] = Math.max(extrema[x][1], points.x1, points.x2);
+				extrema[y][0] = Math.min(extrema[y][0], points.y1, points.y2);
+				extrema[y][1] = Math.max(extrema[y][1], points.y1, points.y2);
+
+				Object.entries(points).forEach(([attr, num]) => {
+					elLine.setAttribute(attr, num);
+				});
+				elLine.classList.add('checkpoint');
+				if (i === 0) {
+					elLine.classList.add('start-line');
+				}
+				this.gTrack.appendChild(elLine);
+				d3.select('svg #gTrack').append('circle')
+					.attr('cx', grad.x).attr('cy', grad.y)
+					.attr('r', i === 0 ? 1.5 : 2).attr('fill', grad.color || 'whitesmoke')
+					.attr('strokeWidth', i === 0 ? '0.5px' : '0')
+					.attr('stroke', i === 0 ? 'white' : 'none');
+
+				// TODO: Calculate borders of piece for geofencing
+			});
+
+			this.extrema = extrema;
+
+			// Draw railings along track
+			railPoints.forEach((rail, j) => {
+				rail.push(rail[0]);
+				const elLine = document.createElementNS(SVG, 'path');
+				const d = [];
+				rail.forEach((point, i) => {
+					if (i === 0) {
+						d.push(`M${point.is.join(',')}`);
+					} else {
+						d.push(`C${[
+							rail[i - 1].after.join(' '),
+							point.before.join(' '),
+							point.is.join(' '),
+						].join(',')}`);
+					}
+				});
+				elLine.setAttribute('d', d.join(''));
+				this.gTrack.appendChild(elLine);
+				this.rails[j] = elLine;
+			});
+
+			// Adjust SVG View Box
+			const buffer = 10;
+			let width = extrema[x][1] - extrema[x][0] + buffer * 2;
+			let height = extrema[y][1] - extrema[y][0] + buffer * 2;
+			let x0 = extrema[x][0] - buffer;
+			let y0 = extrema[y][0] - buffer;
+
+			if (height > width * 4 / 5) {
+				width = height * 5 / 4;
+				x0 = (extrema[x][0] + extrema[x][1] - width) / 2;
+			} else {
+				height = width * 4 / 5;
+				y0 = (extrema[y][0] + extrema[y][1] - height) / 2;
+			}
+
+			this.svg.setAttribute('viewBox', `${x0} ${y0} ${width} ${height}`);
+
+			// Center line around track
+			d3.select('svg').append('g').selectAll('path').data([
+				centerPoints,
+			]).enter().append('path').attr(
+				'd',
+				d3.line().curve(d3.curveBasisClosed)
+			).classed('lane-line', true);
+
+			return this;
+		},
+	},
+	setCars: {
+		enumerable: true,
+		value(cars) {
+			d3.select('svg #gCars').selectAll('circle')
+				.data(cars).enter().append('circle').classed('car', true)
+				.attr('fill', d => d.color).attr('stroke', d => d.color2)
+				.attr('stroke-width', car => car.strokeWidth)
+				.attr('r', car => car.r).attr('cx', d => d.x).attr('cy', d => d.y);
+			this.simulation.nodes(cars);
+			this.cars = cars.slice();
+			return this;
+		},
+	},
+
+	listCars: {
+		enumerable: true,
+		value() {
+			let leadPosition = [];
+
+			const ol = document.querySelector('ol#positions');
+			this.cars.slice().sort((a, b) => {
+				if (a.time.length === 0 && b.time.length === 0) return 0;
+				if (a.time.length === 0) return 1;
+				if (b.time.length === 0) return -1;
+				if (a.time.length > b.time.length) return -1;
+				if (a.time.length < b.time.length) return 1;
+				if (a.time[a.time.length - 1].time > b.time[b.time.length - 1].time) return 1;
+				if (a.time[a.time.length - 1].time < b.time[b.time.length - 1].time) return -1;
+				return 0;
+			}).forEach((car, i) => {
+				if (!(car.elPositionList instanceof Element)) {
+					car.elPositionList = document.createElement('li');
+
+					const elName = document.createElement('span');
+					elName.classList.add('name');
+					elName.innerText = car.name;
+					car.elPositionList.appendChild(elName);
+
+					const elTime = document.createElement('span');
+					elTime.classList.add('time');
+					car.elPositionList.appendChild(elTime);
+				}
+				const li = car.elPositionList;
+
+				if (car.time.length > 1) {
+					const lastTime = car.time.slice().pop();
+
+					// TODO: Why is this sometimes not grabbing the lead position object?!
+					// Get times of the lead car
+					if (i === 0) {
+						leadPosition = car.time.slice();
+					}
+
+					const elTime = li.querySelector('.time');
+					if (i === 0) {
+						const time = ((lastTime.time - car.time[0].time) / 1000).toFixed(2);
+						if (elTime.innerText !== time) elTime.innerText = time;
+						elTime.classList.remove('fade-out');
+					} else {
+						// TODO: Why is this sometimes not grabbing the lead position object?!
+						const leadTime = leadPosition.filter(t => t.lap === lastTime.lap && t.piece === lastTime.piece)
+							.sort((a, b) => a.time - b.time)[0];
+						if (!lastTime) console.error('lastTime:', lastTime);
+						if (!leadTime) console.error('leadTime:', leadTime);
+						let time = (lastTime.time - leadTime.time) / 1000;
+						if (time > 0) time = `+${time.toFixed(2)}`;
+						else time = time.toFixed(2);
+						if (elTime.innerText !== time) {
+							elTime.classList.remove('fade-out');
+							if (car.lapTimes.length < this.laps) setTimeout(() => {
+								elTime.classList.add('fade-out');
+							}, 1000);
+							elTime.innerText = time;
+						}
+					}
+				}
+				li.style.order = i;
+				li.setAttribute('order', i + 1);
+				if (!ol.contains(li)) ol.appendChild(li);
+			});
+			return this;
+		},
+	},
+});
+
+module.exports = RaceTrack;
---
+++ src/js/forceRailingBounce.js
@@ -0,0 +1,179 @@
+const x = 0;
+const y = 1;
+const z = 2;
+
+function forceRailingBounce(rails) {
+	let nodes = [];
+	function force(alpha) {
+		rails.forEach((rail) => {
+			nodes.forEach((car) => {
+				const cp = closestPoint(rail, car);
+				// Bounce!
+				// If car overlaping railing or car will run through railing
+				// TODO: Need to construct force similar to d3.forceCollide to prevent bouncing off virtual/invisible railings
+				if (cp.distance <= car.radius || cp.distance <= Math.hypot(car.vx / 2, car.vy / 2)) {
+					// Vector normal to the surface at this point
+					let normal = [
+						cp.after.y - cp.best.y,
+						cp.best.x - cp.after.x,
+					];
+					const N = Math.hypot(...normal);
+					normal = normal.map(d => d / N);
+
+					// Bounce only if car is moving into rail
+					const position = [
+						car.x - cp.best.x,
+						car.y - cp.best.y,
+					];
+
+					// Point normal in same direction from the railing as the car
+					if (Math.acos((position[x] * normal[x] + position[y] * normal[y]) / Math.hypot(...position)) >= Math.PI / 2) {
+						// Normal is pointed in the wrong direction!
+						normal[x] = -normal[x];
+						normal[y] = -normal[y];
+					}
+
+					// We need to push this far off the railing
+					// x' component of velocity vector
+					const vxn = car.vx * normal[x] + car.vy * normal[y];
+
+					if (Math.acos(vxn / Math.hypot(car.vx, car.vy)) < Math.PI / 2) {
+						// We're already moving in the correct direction!
+						return;
+					}
+
+					let bounceStrength = Math.random() + 1;
+					let delta = [];
+					let d = 0;
+
+					do {
+						bounceStrength += 0.05;
+						delta = [
+							bounceStrength * vxn * normal[x],
+							bounceStrength * vxn * normal[y],
+						];
+
+						d = Math.hypot(
+							car.x + car.vx - delta[x] - cp.after.x,
+							car.y + car.vy - delta[y] - cp.after.y
+						);
+
+						// If running along the rail, try to push off
+					} while (
+						Math.abs((car.vx - delta[x]) * normal[x] + (car.vy - delta[y]) * normal[y]) <= Math.abs(vxn)
+						&& d < car.radius
+						&& bounceStrength < 3
+					);
+
+					// If bounceStrength < 2, there is a loss of velocity
+					// Compensate for loss of x' with gain in y' to maintain (near-)same velocity
+					if (bounceStrength < 2) {
+						// Get tangent pointing in direction of forward movement
+						const tangent = [
+							-normal[y],
+							normal[x],
+						];
+						if (Math.acos(
+							(car.vx * tangent[x] + car.vy * tangent[y]) / Math.hypot(car.vx, car.vy)
+						) > Math.PI / 2) {
+							tangent[x] *= -1;
+							tangent[y] *= -1;
+						}
+
+						// y' component of velocity vector
+						const vyn = car.vx * tangent[x] + car.vy * tangent[y];
+						bounceStrength = 0.1;
+
+						// Don't want to lose too much speed from bounce
+						while (Math.hypot(car.vx - delta[x], car.vy - delta[y]) < 0.6 * Math.hypot(car.vx, car.vy) && bounceStrength < 1) {
+							bounceStrength += 0.1;
+							delta[x] -= 0.1 * vyn * tangent[x];
+							delta[y] -= 0.1 * vyn * tangent[y];
+						}
+
+						// But don't want to accelerate either
+						while (Math.hypot(car.vx - delta[x], car.vy - delta[y]) > Math.hypot(car.vx, car.vy)) {
+							delta[x] += 0.05 * vyn * tangent[x];
+							delta[y] += 0.05 * vyn * tangent[y];
+						}
+					}
+
+					// Finally, apply change in velocity
+					car.vx -= delta[x];
+					car.vy -= delta[y];
+				}
+			});
+		});
+	}
+
+	force.initialize = (_) => {
+		nodes = _;
+	};
+
+	return force;
+}
+
+function closestPoint(pathNode, point) {
+	const nextPoint = [
+		point.x + point.vx,
+		point.y + point.vy,
+	];
+	const dist = p => Math.hypot(p.x - nextPoint[x], p.y - nextPoint[y]);
+	const pathLength = pathNode.getTotalLength();
+	let precision = 16;
+	let best;
+	let bestLength;
+	let bestDistance = Number.POSITIVE_INFINITY;
+
+	// Linear scan for coarse approximation
+	for (let scanLength = 0; scanLength <= pathLength; scanLength += precision) {
+		const scan = pathNode.getPointAtLength(scanLength);
+		const scanDistance = dist(scan);
+		if (scanDistance < bestDistance) {
+			best = scan;
+			bestLength = scanLength;
+			bestDistance = scanDistance;
+		}
+	}
+
+	// Too far away to bother calculating any closer
+	if (bestDistance >= point.radius * 2 && bestDistance >= Math.hypot(point.vx, point.vy)) {
+		return {
+			distance: bestDistance,
+		};
+	}
+
+	let after; // Need this to find the vector normal to the surface
+
+	// Binary search for precise estimate
+	precision /= 2;
+	while (precision > 0.5) {
+		const beforeLength = bestLength - precision;
+		const before = pathNode.getPointAtLength(beforeLength);
+		const beforeDistance = dist(before);
+
+		const afterLength = bestLength + precision;
+		after = pathNode.getPointAtLength(afterLength);
+		const afterDistance = dist(after);
+
+		if (beforeLength >= 0 && beforeDistance < bestDistance) {
+			best = before;
+			bestLength = beforeLength;
+			bestDistance = beforeDistance;
+		} else if (afterLength <= pathLength && afterDistance < bestDistance) {
+			best = after;
+			bestLength = afterLength;
+			bestDistance = afterDistance;
+		} else {
+			precision /= 2;
+		}
+	}
+
+	return {
+		distance: bestDistance,
+		after,
+		best,
+	};
+}
+
+module.exports = forceRailingBounce;
